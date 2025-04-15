09fa8f3a5bd77edee9bbc22abb42dfb2dcdc06ff
Manuel Mazunga De Almeida <maalmeid@c1r1s3.42luanda.com>
2025-04-15 15:58:39 +0100

    GradeBook.hpp done

---
+++ fig04_08/GradeBook.hpp
@@ -0,0 +1,17 @@
+
+
+#include <string>
+
+using st::string;
+
+
+class {
+    private:
+        string couseName;
+    public:
+        GradeBook( string);
+        void setCourseName();
+        string getCourseName();
+        void displayMessage();
+        void determineClassAverage();
+};
