18e2f2d417a356c87f26ea32aadfab61b5997a3b
Manuel Mazunga De Almeida <maalmeid@c1r8s5.42luanda.com>
2025-04-21 16:29:59 +0100

    fig04_12 only class

---
+++ fig04_12/GradeBook.hpp
@@ -0,0 +1,20 @@
+#ifndef GRADEBOOK_HPP
+# define GRADEBOOK_HPP
+
+#include <string>
+
+using std::string;
+
+class  GradeBook
+{
+    private:
+        string courseName;
+    public:
+        GradeBook( string );
+        void setCourseName( string);
+        string getCourseName()
+        void displayMessage();
+        void determineClassAverage();
+};
+
+#endif
